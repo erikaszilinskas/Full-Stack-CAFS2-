7938610c75f4337b2f888f61631e60e707df8755
Erikas <erikas.zilinskas@gmail.com>
2022-05-25 17:16:19 +0100

    js-task2 added

---
+++ javascript/js-task2.js
@@ -0,0 +1,14 @@
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+let a = parseFloat(getRandomArbitrary(1,10).toFixed(2));
+    b = parseFloat(getRandomArbitrary(1,10).toFixed(2));
+    c = parseFloat(getRandomArbitrary(1,10).toFixed(2));
+    d = parseFloat(getRandomArbitrary(1,10).toFixed(2));
+    e = parseFloat(getRandomArbitrary(1,10).toFixed(2));
+
+console.log('your random number:' + a, b, c, d, e);
+
+console.log('lowest number:' + Math.min(a, b, c, d, e));
+console.log('highest number:' + Math.max(a, b, c, d, e));
