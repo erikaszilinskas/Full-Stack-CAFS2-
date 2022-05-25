2e42b258a91eaad02ecebd9f2feb2c47be072845
Erikas <erikas.zilinskas@gmail.com>
2022-05-25 16:31:20 +0100

    javascript task added

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,3 @@
 let num1 = prompt('enter your first number');
 let num2 = prompt('enter your second number');
-console.log('your result:', +num1 + +num2);
+console.log('your result:' +num1 + +num2);
