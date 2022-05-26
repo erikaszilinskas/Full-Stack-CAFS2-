99ae330fd4723fca13e38fe46c0cf7924151225e
Erikas <erikas.zilinskas@gmail.com>
2022-05-26 16:17:55 +0100

    operator-task added

---
+++ javascript/operators-task.js
@@ -0,0 +1,10 @@
+let x = 50;
+let y = 10;
+
+let sum = x + y;
+let difference = x - y;
+let multiplication = x * y;
+let division = x / y;
+
+console.log(sum, difference, multiplication, division);
+
---
+++ javascript/string.js
@@ -0,0 +1,16 @@
+let strOne = 'Hi';
+let strTwo = "Bro";
+let strThree = `!!!`;
+
+
+
+console.log(strOne,strTwo);
+console.log(strOne + strTwo + strThree);
+console.log(strOne + ' ' + strTwo + strThree);
+console.log(`${strOne} ${strTwo} !`);
+
+
+console.log("I am a \"double quoted\" string inside \"double quotes\„;\nFirstLine;\n\\SecondLine;\nThirdLine.");
+
+
+
