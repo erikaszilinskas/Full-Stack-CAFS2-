d81e0dc2cd4f78eb1085e88bbccb2a09705753ed
Erikas <erikas.zilinskas@gmail.com>
2022-06-13 18:35:53 +0100

    dom events task updated

--- DOM/13-06-2022/app.js
+++ DOM/13-06-2022/app.js
@@ -3,11 +3,13 @@
 const heightOutput = document.querySelector('#height');
 const widthOutput = document.querySelector('#width');
 
-window.addEventListener('resize', windowSize);
-
 function windowSize() {
   heightOutput.textContent = window.innerHeight;
   widthOutput.textContent = window.innerWidth;
 }
+
+window.addEventListener('resize', windowSize);
+
+windowSize();
 
 
