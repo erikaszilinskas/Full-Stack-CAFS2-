e455b01223c35e99a9847c998b7ff5e4b13ce031
Erikas <erikas.zilinskas@gmail.com>
2022-06-13 17:57:24 +0100

    dom events task added

--- DOM/13-06-2022/app.js
+++ DOM/13-06-2022/app.js
@@ -1,3 +1,5 @@
+https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
+
 const heightOutput = document.querySelector('#height');
 const widthOutput = document.querySelector('#width');
 
