189c26df70c12cce43e08aa0aa12f59dba5ab7dc
Erikas <erikas.zilinskas@gmail.com>
2022-06-14 16:39:02 +0100

    calculator-task added

--- DOM/13-06-2022/calculator-task.js
+++ DOM/13-06-2022/calculator-task.js
@@ -1,9 +1,8 @@
 document.getElementById("calculate").addEventListener("click", function () {
-
-    document.getElementById("result").innerText = " ";
     const num1 = parseInt(document.getElementById("num1").value);
     const num2 = parseInt(document.getElementById("num2").value);
     const op = document.getElementById("op").value;
+    document.getElementById("result").innerText = " ";
     document.getElementById("result").innerText += calculate(num1, num2, op)
 })
 
