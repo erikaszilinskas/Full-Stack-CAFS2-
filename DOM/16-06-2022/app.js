764334edc9602d7f44e95f3fd4ebc3d0413c949c
Erikas <erikas.zilinskas@gmail.com>
2022-06-16 20:11:47 +0100

    add element task updated

--- DOM/16-06-2022/app.js
+++ DOM/16-06-2022/app.js
@@ -1,33 +1,29 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-});
 const mainDiv = document.getElementById("main");
 document.body.onload = addElement;
 
-function addElement () {
-  const newH2 = document.createElement("h2");
-  const newContent = document.createTextNode("Hi there and greetings from H2!");
-  newH2.appendChild(newContent);
+function addElement() {
+    const newH2 = document.createElement("h2");
+    const newContent = document.createTextNode("Hi there and greetings from H2!");
+    newH2.appendChild(newContent);
 
-  const newP = document.createElement("p");
-  const newContentP = document.createTextNode("Hi there and greetings from P!");
-  newP.appendChild(newContentP);
+    const newP = document.createElement("p");
+    const newContentP = document.createTextNode("Hi there and greetings from P!");
+    newP.appendChild(newContentP);
 
-  const newUl = document.createElement("ul");
-  
-  const newLi = document.createElement("li");
-  const newContentLi = document.createTextNode("Hi there and greetings!");
-  newLi.appendChild(newContentLi);
+    const newUl = document.createElement("ul");
 
-  const secondP = document.createElement("p");
-  const newContentSecondP = document.createTextNode("second paragraph!");
-  secondP.appendChild(newContentSecondP);
+    const newLi = document.createElement("li");
+    const newContentLi = document.createTextNode("Hi there and greetings!");
+    newLi.appendChild(newContentLi);
 
-  mainDiv.appendChild(newH2)
-  mainDiv.appendChild(newP)
-  mainDiv.appendChild(newUl)
-  mainDiv.appendChild(secondP)
-  newUl.appendChild(newLi)
+    const secondP = document.createElement("p");
+    const newContentSecondP = document.createTextNode("second paragraph!");
+    secondP.appendChild(newContentSecondP);
+
+    mainDiv.appendChild(newH2)
+    mainDiv.appendChild(newP)
+    mainDiv.appendChild(newUl)
+    mainDiv.appendChild(secondP)
+    newUl.appendChild(newLi)
 }
 
