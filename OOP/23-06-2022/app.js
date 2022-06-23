9ab4b1fb5a4392b04b8765d214c3310719b94fa2
Erikas <erikas.zilinskas@gmail.com>
2022-06-23 16:27:17 +0100

    OOP movies task added

---
+++ OOP/23-06-2022/app.js
@@ -0,0 +1,11 @@
+let movie1 = new Movies('King Richard', 2021, '	Reinaldo Marcus Green', 50000000, 38000000 );
+
+console.log(movie1);
+console.log(movie1.getIntroduction());
+console.log(movie1.getProfit());
+
+let movie2 = new Movies('I Am Legend', 2007, 'Francis Lawrence', 150000000, 585000000 );
+
+console.log(movie2);
+console.log(movie2.getIntroduction());
+console.log(movie2.getProfit());
---
+++ OOP/23-06-2022/movie.js
@@ -0,0 +1,18 @@
+class Movies {
+    constructor(name, year, director, budget, income) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.budget = budget;
+        this.income = income;
+    }
+
+    getIntroduction() {
+        return `Movie introduction:  ${this.name}  was realeased in ${this.year} and the director is ${this.director}`;
+    }
+
+    getProfit() {
+        return `The profit was  ${this.income - this.budget} milion dollars`;
+    }
+
+}
